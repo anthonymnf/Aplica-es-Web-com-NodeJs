97d7141a2f89c90ae28a48a848e66e2a86ff7665
anthonymnf <anthonymnf30@gmail.com>
2024-06-24 10:14:24 -0300

    :sparkles: feat: First server HTTP

---
+++ Primeiro servidor HTTP/server.js
@@ -0,0 +1,27 @@
+const http = require("node:http");
+
+const server = http.createServer((request, response) => {
+  console.log(request);
+  const path = request.url;
+  switch (path) {
+    case "/":
+      response.writeHead(200);
+      response.write("Você está na página inicial!");
+      break;
+    case "/artigos":
+      response.writeHead(200);
+      response.write('Você está na página "artigos"!');
+      break;
+    default:
+      response.writeHead(404);
+      response.write("Caminho não encontrado!");
+      break;
+  }
+  response.end();
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Servidor rodando em: http://localhost:${PORT}/`);
+});
